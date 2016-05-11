8983d4c8e4d2f4ecc620eae71b3f0bc5506840fb
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-05-11 17:38:30 -0500

    formCtrlSpec

--- js/ng/formCtrlSpec.js
+++ js/ng/formCtrlSpec.js
@@ -1,12 +1,12 @@
-describe('Controller: enviar.creepypastas.com.formCtrl', function () {
+describe('Controller: creepypastasApp.formCtrl', function () {
 
-    // load the controller's module
-    beforeEach(module('enviar.creepypastas.com'));
+// load the controller's module
+    beforeEach(module('creepypastasApp'));
 
     var ctrl,
         scope;
 
-    // Initialize the controller and a mock scope
+// Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
         ctrl = $controller('formCtrl', {
