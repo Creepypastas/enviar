99fcdb2927eb0fa8560115e989ef68254c07ec0d
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-05-11 20:23:54 -0500

    Listo: Enviar new-post a creepypastas.com ToDo: Solicitar sus credenciales al usuario ToDo: Manejar errores y mostrar mensajes útiles al usuario.

--- js/ng/formCtrl.js
+++ js/ng/formCtrl.js
@@ -8,10 +8,39 @@
  * @requires $scope
  * */
 angular.module('creepypastasApp', [])
-    .controller('formCtrl', function ($scope) {
-        this.post = {
+    .controller('formCtrl', ['$scope', '$http', function ($scope, $http) {
+        var formCtrl = this;
+        formCtrl.post = {
             title: '',
             content: ''
         };
+        formCtrl.currentUser = {
+            username: 'invitado',
+            p: 'invitado'
+        };
 
-    });
+        formCtrl.response = {};
+
+        $scope.submitPost = function () {
+            formCtrl.sendCommand("post-new");
+        };
+
+        formCtrl.sendCommand = function (command) {
+
+            $http({
+                url: 'https://creepypastas.com/comand',
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                data: {user: formCtrl.currentUser, post: formCtrl.post, command: command}
+            }).then(function success(res) {
+                console.log("updatePost::response");
+                console.log(res.data);
+                formCtrl.response = res.data;
+            }, function error(res) {
+                console.error("updatePost::response");
+                console.error(res.data);
+                formCtrl.response = res.data;
+            });
+        };
+
+    }]);
